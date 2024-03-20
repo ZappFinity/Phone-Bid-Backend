444d5d939d459eb83e5bd133595118dc59c33c5c
M Zubair Naeem <zubairnaeem45@gmail.com>
2024-03-20 22:56:34 +0500

    blog api/seeders

---
+++ app/Http/Controllers/Blog/BlogController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Blog;
+
+use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum');
+    }
+    public function index()
+    {
+        $blogs = Blog::all();
+        return response()->json([
+            'success' => true,
+            'message' => 'List of all blogs',
+            'data' => $blogs
+        ]);
+    }
+    public function create()
+    {
+        //
+    }
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(string $id)
+    {
+        $blog = Blog::find($id);
+        return response()->json([
+            'success' => true,
+            'message' => 'Blog details',
+            'data' => $blog
+        ]);
+    }
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,30 +1,28 @@
 <?php
 
+use App\Http\Controllers\Blog\BlogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LogoutController::class, 'logout']);
 
-    Route::group(['middleware' => ['auth:sanctum']], function () {; 
-        //Auth
-        Route::post('/logout', [LogoutController::class, 'logout']);
-        // Route::post('/changePassword', [ChangePasswordController::class, 'changePassword']);
-        // Route::post('/passwordResetEmail', [PasswordResetController::class, 'passwordResetEmail']);
+/**
+ * Blog Routes
+ */
+Route::controller(BlogController::class)->group(function () {
+    Route::prefix('blog')->group(function () {
+        Route::get('/list', [BlogController::class, 'index']);
+        Route::get('/show/{id}', [BlogController::class, 'show']);
     });
+});
+/** 
+ * Default Route
+ */
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});    
+});
